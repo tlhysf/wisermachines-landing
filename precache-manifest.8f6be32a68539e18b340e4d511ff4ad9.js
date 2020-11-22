01a1e9fdad5be9c506392995b540ed0b86fc2e50
Talha Yousuf <talhayousuf.work@gmail.com>
2020-11-22 16:59:18 +0500

    Updates

---
+++ precache-manifest.8f6be32a68539e18b340e4d511ff4ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1436e63814a6ece3d7af6970f69df83b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28a918176416057f627b",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1def542df3adcc3e1d37",
+    "url": "/static/js/2.ef9b312c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ef9b312c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a918176416057f627b",
+    "url": "/static/js/main.2198c37f.chunk.js"
+  },
+  {
+    "revision": "e22fd322f0b45aec7fb4",
+    "url": "/static/js/runtime-main.a7f6340d.js"
+  }
+]);
